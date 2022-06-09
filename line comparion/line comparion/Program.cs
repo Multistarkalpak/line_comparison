fb34d976d65c9def77a3520cc6dbf3b679a6f958
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-06-09 16:55:36 +0530

    Welcome to Line comparison problem

---
+++ line comparion/line comparion/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Welcome to linecomparsion problems");
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // This is the Task UC1 for Welcoming 
+            Console.WriteLine("Welcome to linecomparsion problems");
+
+        }
+    }
+}
